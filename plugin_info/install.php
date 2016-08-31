57e0a69830aa7c27160f2a57f5f0b238ff7b450d
mika-nt28 <michaelhalbout@gmail.com>
2016-08-31 14:14:25 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -3,35 +3,13 @@ require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function Freebox_OS_install() {
 	Freebox_OS::CreateArchi();
-	$cron = cron::byClassAndFunction('Freebox_OS', 'RefreshInformation');
-	if (!is_object($cron)) {
-		$cron = new cron();
-		$cron->setClass('Freebox_OS');
-		$cron->setFunction('RefreshInformation');
-		$cron->setEnable(1);
-		$cron->setSchedule('* * * * *');
-		$cron->save();
-	}
 }
 
 function Freebox_OS_update() {
 	Freebox_OS::CreateArchi();    
-	$cron = cron::byClassAndFunction('Freebox_OS', 'RefreshInformation');
-	if (!is_object($cron)) {
-		$cron = new cron();
-		$cron->setClass('Freebox_OS');
-		$cron->setFunction('RefreshInformation');
-		$cron->setEnable(1);
-		$cron->setSchedule('* * * * *');
-		$cron->save();
-	}
 }
 
 function Freebox_OS_remove() {
-	$cron = cron::byClassAndFunction('Freebox_OS', 'RefreshInformation');
-    if (is_object($cron)) {
-        $cron->remove();
-    }
 }
 
 ?>
