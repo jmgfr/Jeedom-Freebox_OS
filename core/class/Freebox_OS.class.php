4c6f8336568ca6da74eb775a828756b5387d861c
jmgfr <jmgfr@users.noreply.github.com>
2016-09-21 10:45:43 +0200

    Update Freebox_OS.class.php

--- core/class/Freebox_OS.class.php
+++ core/class/Freebox_OS.class.php
@@ -136,8 +136,8 @@ class Freebox_OS extends eqLogic {
 	        curl_close($ch);
 	        $result=json_decode($content, true);
 		log::add('Freebox_OS','debug', $content);
-		if(isset($result['result']["error_code"]))
-			if($result['result']["error_code"]=="auth_required")
+		if(isset($result["error_code"]))
+			if($result["error_code"]==="auth_required")
 				self::deamon_stop();
 		return $result;	
     	}
